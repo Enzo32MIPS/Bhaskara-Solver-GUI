03efaa2ef9372d9801eda8e3904a7c39cc8d10c6
Enzo32MIPS <enzomoraes.aluno@unipampa.edu.br>
2025-08-26 11:38:00 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main{
+	public static void main(String[] args){
+
+		JFrame frame = new JFrame("Calculadora Bhaskara");
+		frame.setSize(440,240);
+		frame.setLayout(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		System.out.println("Window successfully created");
+
+		JLabel labelA = new JLabel("X²+");
+		labelA.setBounds(130,40,20,25);
+		frame.add(labelA);
+		JTextField fieldA = new JTextField();
+		frame.add(fieldA);
+		fieldA.setBounds(75,40,50,25);
+		System.out.println("LabelA and FieldA loaded");
+
+		JLabel labelB = new JLabel("X+");
+		frame.add(labelB);
+		labelB.setBounds(255,40,50,25);
+		JTextField fieldB = new JTextField();
+		frame.add(fieldB);
+		fieldB.setBounds(195,40,50,25);
+		System.out.println("LabelB and FieldB loaded");
+
+		JTextField fieldC = new JTextField();
+		frame.add(fieldC);
+		fieldC.setBounds(305,40,50,25);
+		System.out.println("FieldC loaded");
+
+		JButton sbutton = new JButton("Solucionar");
+		sbutton.setBounds(170, 90, 100, 30);
+		frame.add(sbutton);
+
+		JLabel labelres = new JLabel("X1 = ?   X2 = ?");
+		labelres.setBounds(185,130,110,25);
+		frame.add(labelres);
+
+		frame.setVisible(true);
+
+		sbutton.addActionListener(e -> {
+
+			String temp1 = fieldA.getText(); 
+			String temp2 = fieldB.getText();
+			String temp3 = fieldC.getText();
+
+			if(!temp1.isEmpty() && !temp2.isEmpty() && !temp3.isEmpty()){
+
+				double aa, bb, cc, temp,x1,x2;
+
+				aa = Double.parseDouble(fieldA.getText());
+				bb = Double.parseDouble(fieldB.getText());
+				cc = Double.parseDouble(fieldC.getText());
+
+				temp = (bb*bb)-(4*aa*cc);
+				temp = Math.sqrt(temp);
+
+				x1 = (-bb+temp)/(2*aa);
+				x2 = (-bb-temp)/(2*aa);
+
+				labelres.setText("X1 = " + x1 + " X2 = " +x2);
+				String tempo = labelres.getText();
+				int length = tempo.length();
+				labelres.setBounds((185-length), 130, 110, 25);
+
+			} else {
+				labelres.setText("Preencha os campos");
+				String temp5 = labelres.getText();
+				int length2 = temp5.length();
+				labelres.setBounds((175-length2),130,150,25);
+			}
+
+		});
+	}
+}
