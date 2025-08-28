7dc120acaf1fbc855435c3e6a8e0962e932b2e17
Enzo32MIPS <enzomoraes.aluno@unipampa.edu.br>
2025-08-28 11:25:14 -0300

    Added debug commentary

--- Main.java
+++ Main.java
@@ -14,7 +14,6 @@ public class Main{
     		System.err.println("Failed to initialize FlatLaf");
 		}
 
-		//Color customColor = new Color(40,43,48);
 		ImageIcon customIcon = new ImageIcon(Main.class.getResource("/quill-pen-fill.png"));
 		JFrame frame = new JFrame("Calculadora Bhaskara");
 		frame.setSize(440,240);
@@ -23,14 +22,13 @@ public class Main{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
-		//frame.getContentPane().setBackground(customColor);
 		System.out.println("Window successfully created");
+
 
 		JLabel labelA = new JLabel("X²+");
 		labelA.setBounds(165,40,20,25);
 		frame.add(labelA);
 		JTextField fieldA = new JTextField();
-		//labelA.setForeground(Color.WHITE);
 		frame.add(fieldA);
 		fieldA.setBounds(125,40,30,25);
 		System.out.println("LabelA and FieldA loaded");
@@ -39,7 +37,6 @@ public class Main{
 		frame.add(labelB);
 		labelB.setBounds(235,40,30,25);
 		JTextField fieldB = new JTextField();
-		//labelB.setForeground(Color.WHITE);
 		frame.add(fieldB,BorderLayout.CENTER);
 		fieldB.setBounds(195,40,30,25);
 		System.out.println("LabelB and FieldB loaded");
@@ -51,12 +48,10 @@ public class Main{
 
 		JButton sbutton = new JButton("Solucionar");
 		sbutton.setBounds(160, 90, 100, 30);
-		//sbutton.setForeground(Color.WHITE);
 		frame.add(sbutton,BorderLayout.CENTER);
 
 		JLabel labelres = new JLabel("X1 = ?   X2 = ?");
 		labelres.setBounds(175,130,110,25);
-		//labelres.setForeground(Color.WHITE);
 		frame.add(labelres);
 
 		String[] items = {"Light mode", "Dark mode"};
@@ -87,12 +82,15 @@ public class Main{
 				x1 = (-bb+temp)/(2*aa);
 				x2 = (-bb-temp)/(2*aa);
 
+				System.out.println("X1 = " + x1 + "\nX2 = " +x2);
+
 				labelres.setText("X1 = " + x1 + " X2 = " +x2);
 				String tempo = labelres.getText();
 				int length = tempo.length();
 				labelres.setBounds((185-length), 130, 200, 25);
 
 			} else {
+				System.out.println("Error: no data");
 				labelres.setText("Preencha os campos");
 				String temp5 = labelres.getText();
 				int length2 = temp5.length();
@@ -104,25 +102,21 @@ public class Main{
             String selected = (String) dropdown.getSelectedItem();
 			if (selected == "Dark mode"){
 				try{
-					//frame.setVisible(false);
 					UIManager.setLookAndFeel(new FlatDarkLaf());
-					//frame.setVisible(true);
+					System.out.println("Changed theme to: Dark");
 				} catch (Exception ex){
 					System.err.println("Failed to change themes");
 				}
 			} else {
 				try {
-					//frame.setVisible(false);
 					UIManager.setLookAndFeel(new FlatLightLaf());
-					//frame.setVisible(true);
+					System.out.println("Changed theme to: Light");
 				} catch (Exception ex){
 					System.err.println("Failed to change themes");
 				}
 			}
 
 			SwingUtilities.updateComponentTreeUI(frame);
-
-            //JOptionPane.showMessageDialog(frame, "You selected: " + selected);
         });
 	}
 }
